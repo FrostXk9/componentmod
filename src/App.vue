bbc40089730234bceb4044b96a18e92f49784f08
FrostXk9 <brandonroulstone465@gmail.com>
2024-01-23 21:06:56 +0200

    not completed,landing txt needed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
+  <NavbarComp />
+  
+
   <router-view/>
 </template>
-
+<script>
+import NavbarComp from './components/NavigationBar.vue';
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components : {
+    NavbarComp
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -13,6 +24,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-image: url("https://cdn-images.imagevenue.com/b5/23/64/ME17J4GH_o.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
 }
 
 nav {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,37 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" id="landingPage">
+    
+    <div class="col-md-6 p-lg-5 mx-auto my-5">
+
+      <h1 class="display-3 fw-bold">Designed for engineers</h1>
+
+      <h3 class="fw-normal text-black mb-3">Build anything you want with Aperture</h3>
+
+      <div class="d-flex gap-3 justify-content-center lead fw-normal">
+
+        <ButtonComp />
+
+      </div>
+
+    </div>
+    
   </div>
+
 </template>
 
 <script>
+import ButtonComp from './ButtonComp.vue';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components : {
+    ButtonComp
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 h3 {
   margin: 40px 0 0;
 }
@@ -54,7 +43,11 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
+#landingPage{
+  background-color: rgba(0, 0, 0, 0);
+}
 a {
   color: #42b983;
 }
+
 </style>
